f23f76febc0c0465cd7c051484096b004153d436
bohdan <dev2297@outlook.com>
2022-10-19 07:23:11 +0200

    fix bootstrap and add vue-router,vue-loader

--- resources/views/first.blade.php
+++ resources/views/first.blade.php
@@ -10,15 +10,15 @@
                     <input type="text" class="col-md-8 mx-auto form-control search-form-control ms-lg-auto" placeholder="Search a ticker or company name"  autocomplete="off" maxlength="240" aria-label="Search" aria-controls="top-search" data-controller="s-popover" data-action="focus->s-popover#show" data-s-popover-placement="bottom-start" aria-expanded="true"/>
                 </div>
                 <div class="list-item">
-                    <h5>30+years of financial data</h5>
+                    <a href=""><h5>30+years of financial data</h5></a>
                     <p>comprehensive financial data</p>
                 </div>
                 <div class="list-item">
-                    <h5>100,000+ companies</h5>
+                    <a href=""><h5>100,000+ companies</h5></a>
                     <p>Globally Covered Stocks</p>
                 </div>
                 <div class="list-item">
-                    <h5>Tons of features</h5>
+                    <a href=""><h5>Tons of features</h5></a>
                     <p>Screener, Forum, Articles & Guru Tracker</p>
                 </div>
             </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,6 @@
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
-        <script src="{{ asset('bootstrap/js/bootstrap.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 ">
