39479e8cb3cd3bc0c297f3657622a2693736f72b
bohdan <dev2297@outlook.com>
2022-11-15 21:21:51 +0100

    fix Maincontroller

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -30,8 +30,8 @@ class MainController extends Controller
     
     public function first(){
         $stock = Eod::exchange();
-        // $content = $stock->exchange_list()->json();
-        // var_dump($content);
+        $content = $stock->exchange_list()->json();
+        var_dump($content);
         return view('first');
     }
 
