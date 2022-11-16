826cfe5204a309ae47c89965c1e3e0d51c72ce19
bohdan <dev2297@outlook.com>
2022-11-16 23:53:05 +0100

    aa

--- public/assets/js/pages/first.js
+++ public/assets/js/pages/first.js
@@ -1,28 +1,30 @@
 $(document).ready(function(){
     $(".search-input").keyup(function(){
-        $.ajax({
-            type: "GET",
-            url: '/search/' + $(this).val(),
-            headers: {
-                "X-CSRF-TOKEN": $('#_token').val()
-            },
-            dataType: 'json',
-            success: function(data) {
-                
-                $('ul#search-result').children().remove();
-                base_url = $('.base_url').val();
-                var html='';
-                for(i=0 ; i < data.result.length ; i++){
-                    html += '<li class="d-flex bg-hover-primary ">';
-                    html += '<a href="'+base_url+'/'+data.result[i].Code+'" class="flex fs-6 text-gray-700">';
-                    html += '<div class="w-125px w-md-150px py-1  text-left"><span class="ml-2">'+data.result[i].code+'</span></div>';
-                    html += '<div class="flex-1  px-1 lg:px-2 py-1 text-left border-l-2 turncate"><span class="ml-2">'+data.result[i].Name+'</span></div>';
-                    html += '<div class="flex  px-1 lg:px-2 py-1 text-right items-center turncate self-center text-gray-400"><span class="ml-4">'+data.result[i].Exchange+'</span></div>';
-                    html += '</a></li>';
+        if($(this).val().length > 0){
+            $.ajax({
+                type: "GET",
+                url: '/search/' + $(this).val(),
+                headers: {
+                    "X-CSRF-TOKEN": $('#_token').val()
+                },
+                dataType: 'json',
+                success: function(data) {
+                    
+                    $('ul#search-result').children().remove();
+                    base_url = $('.base_url').val();
+                    var html='';
+                    for(i=0 ; i < data.result.length ; i++){
+                        html += '<li class="d-flex bg-hover-primary ">';
+                        html += '<a href="'+base_url+'/'+data.result[i].Code+'" class="flex fs-6 text-gray-700">';
+                        html += '<div class="w-125px w-md-150px py-1  text-left"><span class="ml-2">'+data.result[i].code+'</span></div>';
+                        html += '<div class="flex-1  px-1 lg:px-2 py-1 text-left border-l-2 turncate"><span class="ml-2">'+data.result[i].Name+'</span></div>';
+                        html += '<div class="flex  px-1 lg:px-2 py-1 text-right items-center turncate self-center text-gray-400"><span class="ml-4">'+data.result[i].Exchange+'</span></div>';
+                        html += '</a></li>';
+                    }
+                    $('ul#search-result').append(html);
                 }
-                $('ul#search-result').append(html);
-            }
-        });
+            });
+        }
     });
     
 });
