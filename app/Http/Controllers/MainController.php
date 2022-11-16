adeffe12067ed58eb1e835e99e399181ec4e4518
bohdan <dev2297@outlook.com>
2022-11-16 22:38:10 +0100

    33

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -49,17 +49,17 @@ class MainController extends Controller
         // var_dump($data);
         usort($data, function ($a, $b) use ($name) {
             // find the term in first entry
-            $t1 = preg_match("/^.*?\b($name\w*)\b.*\$/i", $a['Name'], $matches) ? $matches[1] : '';
+            $t1 = preg_match("/^.*?\b($name\w*)\b.*\$/i", $a->Name, $matches) ? $matches[1] : '';
            
             // find the term in second entry
-            $t2 = preg_match("/^.*?\b($name\w*)\b.*\$/i", $b['Name'], $matches) ? $matches[1] : '';
+            $t2 = preg_match("/^.*?\b($name\w*)\b.*\$/i", $b->Name, $matches) ? $matches[1] : '';
             // check if the terms were found
             if ($t1 == '' && $t2 != '') return 1;
             if ($t1 != '' && $t2 == '') return -1;
             // found in both - if not the same, just sort on the keyword
             if ($t1 != $t2) return strcmp($t1, $t2);
             // found the same keyword, sort on the whole title
-            return strcmp($a['Name'], $b['Name']);
+            return strcmp($a->Name, $b->Name);
         });
 
         $result=array();
