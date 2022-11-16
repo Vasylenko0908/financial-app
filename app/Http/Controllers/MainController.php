6ed593366507c81a8f6cad19b91cca52caed65ee
bohdan <dev2297@outlook.com>
2022-11-16 21:06:29 +0100

    test

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -45,7 +45,7 @@ class MainController extends Controller
     {
         //
         $data = json_decode($this->exchange_symbol);
-
+        var_dump($data);
         usort($data, function ($a, $b) use ($name) {
             // find the term in first entry
             $t1 = preg_match("/^.*?\b($name\w*)\b.*\$/i", $a['Name'], $matches) ? $matches[1] : '';
