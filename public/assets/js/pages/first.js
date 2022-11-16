1886dd89686fe6e7d3f63c8a78e51465a7e87e31
bohdan <dev2297@outlook.com>
2022-11-16 23:22:49 +0100

    k

--- public/assets/js/pages/first.js
+++ public/assets/js/pages/first.js
@@ -7,17 +7,17 @@ $(document).ready(function(){
                 "X-CSRF-TOKEN": $('#_token').val()
             },
             dataType: 'json',
-            success: function(result) {
+            success: function(data) {
                 alert(result);
                 $('ul#search-result').children().remove();
                 base_url = $('.base_url').val();
                 var html='';
-                for(i=0 ; i < result.length ; i++){
+                for(i=0 ; i < data.result.length ; i++){
                     html += '<li class="d-flex bg-hover-primary ">';
-                    html += '<a href="'+base_url+result[i].code+'" class="flex fs-6 text-gray-700">';
-                    html += '<div class="w-125px w-md-150px py-1  text-left"><span class="ml-2">'+result[i].code+'</span></div>';
-                    html += '<div class="flex-1  px-1 lg:px-2 py-1 text-left border-l-2 turncate"><span class="ml-2">'+result[i].Name+'</span></div>';
-                    html += '<div class="flex  px-1 lg:px-2 py-1 text-right items-center turncate self-center text-gray-400"><span class="ml-4">'+result[i].Exchange+'</span></div>';
+                    html += '<a href="'+base_url+'/'+data.result[i].code+'" class="flex fs-6 text-gray-700">';
+                    html += '<div class="w-125px w-md-150px py-1  text-left"><span class="ml-2">'+data.result[i].code+'</span></div>';
+                    html += '<div class="flex-1  px-1 lg:px-2 py-1 text-left border-l-2 turncate"><span class="ml-2">'+data.result[i].Name+'</span></div>';
+                    html += '<div class="flex  px-1 lg:px-2 py-1 text-right items-center turncate self-center text-gray-400"><span class="ml-4">'+data.result[i].Exchange+'</span></div>';
                     html += '</a></li>';
                 }
                 $('ul#search-result').append(html);
