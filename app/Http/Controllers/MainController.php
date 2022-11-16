b05a155eee032aa9a1e4df365a0cea7c6f1efddb
bohdan <dev2297@outlook.com>
2022-11-16 22:11:18 +0100

    ss

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -49,8 +49,9 @@ class MainController extends Controller
         // var_dump($data);
         usort($data, function ($a, $b) use ($name) {
             // find the term in first entry
-            $t1 = preg_match("/^.*?\b($name\w*)\b.*\$/i", $a['Name'], $matches) ? $matches[1] : '';
             var_dump($a["Name"]);
+            $t1 = preg_match("/^.*?\b($name\w*)\b.*\$/i", $a['Name'], $matches) ? $matches[1] : '';
+           
             // find the term in second entry
             $t2 = preg_match("/^.*?\b($name\w*)\b.*\$/i", $b['Name'], $matches) ? $matches[1] : '';
             // check if the terms were found
