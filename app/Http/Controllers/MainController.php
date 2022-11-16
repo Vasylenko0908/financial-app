b6c5d57ae1eba6476c648487db8f8fdf985c2d14
bohdan <dev2297@outlook.com>
2022-11-16 18:58:21 +0100

    s

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -48,9 +48,9 @@ class MainController extends Controller
 
         usort($data, function ($a, $b) use ($name) {
             // find the term in first entry
-            $t1 = preg_match("/^.*?\b($name\w*)\b.*\$/i", $a['result_title'], $matches) ? $matches[1] : '';
+            $t1 = preg_match("/^.*?\b($name\w*)\b.*\$/i", $a['Name'], $matches) ? $matches[1] : '';
             // find the term in second entry
-            $t2 = preg_match("/^.*?\b($name\w*)\b.*\$/i", $b['result_title'], $matches) ? $matches[1] : '';
+            $t2 = preg_match("/^.*?\b($name\w*)\b.*\$/i", $b['Name'], $matches) ? $matches[1] : '';
             // check if the terms were found
             if ($t1 == '' && $t2 != '') return 1;
             if ($t1 != '' && $t2 == '') return -1;
