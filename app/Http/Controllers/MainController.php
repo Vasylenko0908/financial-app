d40a92fa8c604f55a51425fc96e2c73ac022eda3
bohdan <dev2297@outlook.com>
2022-11-16 02:40:35 +0100

    fix US

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -30,7 +30,7 @@ class MainController extends Controller
     
     public function first(){
         $stock = Eod::exchange();
-        $content = $stock->exchange_symbol_list('NASDAQ')->json();
+        $content = $stock->exchange_symbol_list('US')->json();
         dd($content);
         return view('first');
     }
