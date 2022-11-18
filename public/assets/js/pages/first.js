39837cc319d213c70706bb4b414ac99bf81fc401
bohdan <dev2297@outlook.com>
2022-11-18 04:27:44 +0100

    ff

--- public/assets/js/pages/first.js
+++ public/assets/js/pages/first.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
                     for(i=0 ; i < data.result.length ; i++){
                         html += '<li class="w-full bg-hover-primary ">';
                         html += '<a href="'+base_url+'/company/'+data.result[i].Code+'" class="flex fs-6 text-gray-700">';
-                        html += '<div class="w-125px w-md-100px px-2 lg:px-3 py-1  text-left items-center"><span class="mt-1">'+data.result[i].Code+'</span></div>';
+                        html += '<div class="w-125px w-md-100px px-2 lg:px-3 py-1 turncate text-left items-center"><span class="mt-1">'+data.result[i].Code+'</span></div>';
                         html += '<div class="flex-1  px-2 lg:px-3 py-1 text-left border-l-2 turncate items-center"><span class="mt-1">'+data.result[i].Name+'</span></div>';
                         html += '<div class="flex  px-2 lg:px-3 py-1 text-right items-center turncate self-center"><span class="mt-1">'+data.result[i].Exchange+'</span></div>';
                         html += '<div class="flex px-3 lg:px-3 py-1 items-center self-center"><img class="w-4 h-4 my-auto inline ml-1" src="'+base_url+'/assets/media/flags/united-states.svg'+'"/></div>';
