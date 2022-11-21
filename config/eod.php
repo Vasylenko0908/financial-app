d29e50c3132bff166afde56ee12b3fdbc090d4a0
bohdan <dev2297@outlook.com>
2022-11-21 19:31:42 +0100

    d

--- config/eod.php
+++ config/eod.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    // 'api_token' => '636435d8ba20e3.70779335' // mine
-    'api_token' => '632dc1b8e1fa13.50309434546'    // Asam
+    'api_token' => '636435d8ba20e3.70779335' // mine
+    // 'api_token' => '632dc1b8e1fa13.50309434546'    // Asam
 ];
